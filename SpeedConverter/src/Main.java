0ea7291be850798ef304e741bab413273c18b77a
Andevr <andevelpr@outlook.com>
2019-12-27 08:14:52 -0600

    added SpeedConverter Exercise

---
+++ SpeedConverter/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Miles = " + SpeedConverter.toMilesPerHour(10.5));
+
+        SpeedConverter.printConversion(10.5);
+    }
+}
